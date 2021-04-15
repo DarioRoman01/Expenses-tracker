afabe17c85d727ea9f0d4f1fcfbbe71d1a4fcd68
DarioRoman <darioroman2001@gmail.com>
2021-04-15 17:18:29 -0400

    adding forgot password and change password to the users views.

--- server/views/users.go
+++ server/views/users.go
@@ -1,9 +1,12 @@
 package views
 
 import (
+	"CodePonder/cache"
 	"CodePonder/controllers"
 	"CodePonder/models"
 	"CodePonder/utils"
+	"context"
+	"fmt"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -34,6 +37,9 @@ func (u *UsersHandler) SignupView(c echo.Context) error {
 		return c.JSON(err.Code, err.Message)
 	}
 
+	session := cache.Default(c)
+	session.Set("userId", user.ID)
+	session.Save()
 	return c.JSON(201, usersCreated)
 }
 
@@ -58,6 +64,53 @@ func (u *UsersHandler) LoginView(c echo.Context) error {
 			return c.JSON(err.Code, err.Message)
 		}
 
+		session := cache.Default(c)
+		session.Set("userId", user.ID)
+		session.Save()
+
 		return c.JSON(200, user)
 	}
+}
+
+func (u *UsersHandler) ForgotPasswordView(c echo.Context) error {
+	email := c.FormValue("email")
+	user := usersTable.GetUserByEmail(email, u.DB)
+	if user == nil {
+		return utils.InvalidInput("email", "email does not exist")
+	}
+
+	ok := utils.SendEmail(user)
+
+	if !ok {
+		return echo.NewHTTPError(500, "unable to send email")
+	}
+
+	return c.JSON(200, "we send you and email please check your email")
+}
+
+func (u *UsersHandler) ChangePasswordView(c echo.Context) error {
+	newPassword := c.FormValue("newPassword")
+	if len(newPassword) < 4 {
+		return utils.InvalidInput("newPassword", "password must be at least 4 characters")
+	}
+
+	ctx := context.Background()
+	token := c.FormValue("token")
+	redis := cache.Client()
+	userId := redis.Get(ctx, fmt.Sprintf("forgot-password:%s", token))
+
+	if userId.Val() == "" {
+		return utils.InvalidInput("token", "invalid token")
+	}
+
+	user, err := usersTable.ChangePassword(userId.Val(), newPassword, u.DB)
+	if err != nil {
+		return c.JSON(err.Code, err.Message)
+	}
+
+	session := cache.Default(c)
+	session.Set("userId", user.ID)
+	session.Save()
+
+	return c.JSON(200, "password changed succesfully")
 }
