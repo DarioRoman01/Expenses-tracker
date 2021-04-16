31cffe8c0cc1f6d2c0d9b101dbb29d3492058c17
DarioRoman <darioroman2001@gmail.com>
2021-04-16 11:07:42 -0400

    adding cors config to the server.

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"CodePonder/cache"
 	"CodePonder/routes"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,6 +15,10 @@ func main() {
 	store := cache.SessionsClient()
 
 	// middlewares
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{os.Getenv("CORS_ORIGIN")},
+		AllowCredentials: true,
+	}))
 	e.Use(cache.Sessions("qid", *store))
 	e.Use(middleware.RemoveTrailingSlash())
 
