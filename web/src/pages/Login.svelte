bf8717e4ff9456d5c108e374852d06e94214593f
DarioRoman <darioroman2001@gmail.com>
2021-04-16 18:54:06 -0400

    created login page.

--- web/src/pages/Login.svelte
+++ web/src/pages/Login.svelte
@@ -1,39 +1,114 @@
 <script lang="ts">
-  import {goto} from "@roxi/routify"
+  import Navbar from "../components/Navbar.svelte";
+  import { redirect } from "@roxi/routify";
+
   let usernameOrEmail: string;
   let password: string;
   let error: Error;
 
   async function login() {
     try {
-      const res = await fetch("http://localhost:1323/register", {
-        method: "POST",
+      const res = await fetch("http://localhost:1323/login", {
+        method: "post",
         body: JSON.stringify({
-          usernameOrEmail,
-          password
+          usernameOrEmail: usernameOrEmail,
+          password: password,
         }),
         headers: {
-          "Content-type": "application/json"
-        }
-      }) 
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
       if (res.ok) {
-        $goto("./home")
+        $redirect("./home");
       } else {
-        error = new Error("there was an error :(")
+        const err = await res.json();
+        error = new Error(err.message);
       }
-    } catch(err) {
-      console.log(err)
-      error = new Error("there was an error :(")
+    } catch (err) {
+      error = new Error(err);
     }
   }
-
 </script>
 
+<Navbar />
 <main>
-  <input type="text" bind:value={usernameOrEmail} placeholder="Username">
-  <input type="password" bind:value={password} placeholder="Password">
-  {#if error}
-    <p>{error.message}</p>
-  {/if}
-  <button onclick={login}>Login</button>
+  <form action="#" on:submit={login}>
+    <h1>Login Form</h1>
+    <div class="formcontainer">
+      <hr />
+      <div class="container">
+        <label for="uname"><strong>Username</strong></label>
+        <input
+          type="text"
+          placeholder="Enter Username"
+          bind:value={usernameOrEmail}
+        />
+        <label for="psw"><strong>Password</strong></label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          bind:value={password}
+        />
+        {#if error}
+          <p>{error.message}</p>
+        {/if}
+      </div>
+      <button type="submit">Login</button>
+      <div class="container" style="background-color: #eee">
+        <span class="psw"><a href="/"> Forgot password?</a></span>
+      </div>
+    </div>
+  </form>
 </main>
+
+<style>
+  main {
+    margin-left: 20%;
+    margin-right: 20%;
+  }
+  form {
+    border: 5px solid #f1f1f1;
+  }
+
+  input[type="text"],
+  input[type="password"] {
+    width: 100%;
+    padding: 16px 8px;
+    margin: 10px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-sizing: border-box;
+  }
+
+  button {
+    background-color: #003049;
+    color: white;
+    padding: 14px 0;
+    margin: 10px 0;
+    border: none;
+    border-radius: 10px;
+    width: 100%;
+  }
+  h1 {
+    text-align: center;
+    font-size: 18;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+
+  .formcontainer {
+    text-align: left;
+    margin: 24px 50px 12px;
+  }
+
+  .container {
+    padding: 16px 0;
+    text-align: center;
+  }
+</style>
--- web/src/pages/Register.svelte
+++ web/src/pages/Register.svelte
@@ -8,36 +8,38 @@
   async function register() {
     error = undefined;
     try {
-      const res = await fetch("http://localhost:1323/login", {
+      const res = await fetch("http://localhost:1323/signup", {
         method: "POST",
         body: JSON.stringify({
           username,
           email,
-          password
+          password,
         }),
         headers: {
-          "Content-type": "application/json"
-        }
-      })
-  
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
       if (res.ok) {
-        $goto('./home')
+        $goto("./home");
       } else {
-        error = new Error("there was an error :(")
+        const err = await res.json();
+        error = new Error(err);
       }
-    } 
-    catch(err) {
-      console.log(err)
-      error = new Error("something wrong happend :(")
+    } catch (err) {
+      console.log(err);
+      error = new Error("something wrong happend :(");
     }
   }
 </script>
 
 <div>
   <h1>Register</h1>
-  <input type="text" bind:value={username} placeholder="Username">
-  <input type="email" bind:value={email} placeholder="Email">
-  <input type="password" bind:value={password} placeholder="Password">
+  <input type="text" bind:value={username} placeholder="Username" />
+  <input type="email" bind:value={email} placeholder="Email" />
+  <input type="password" bind:value={password} placeholder="Password" />
   {#if error}
     <p>{error.message}</p>
   {/if}
