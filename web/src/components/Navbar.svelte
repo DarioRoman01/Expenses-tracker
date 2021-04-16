dda12c4260e51683190341b1d413af443e56fa1a
DarioRoman <darioroman2001@gmail.com>
2021-04-16 12:12:59 -0400

    create navbar component and added to index page.

---
+++ web/src/components/Navbar.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { url, goto } from "@roxi/routify"
+</script>
+
+
+<nav class="nav">
+  <div>
+    <h2 class="navbar-title">Expenses Tracker</h2>
+  </div>
+  <div class="navbar-links">
+    <button class="navbar-login" on:click={$goto("./Login")}>
+      Login
+    </button>
+    <button class="navbar-register" on:click={$goto("./Register")}>
+      Register
+    </button>
+    <!-- <a class="login-link" href={$url("./Login")}>Login</a>
+    <a class="register-link" href={$url("./Register")}>Register</a> -->
+  </div>
+</nav>
+
+
+<style>
+  .nav {
+    max-width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #003049;
+    color: #ffffff;
+    border-bottom: #ffffff 1px solid;
+  }
+
+  .navbar-links {
+    padding-top: 10px;
+    align-content: center;
+    text-decoration: none;
+    font-size: large;
+  }
+
+  .navbar-login {
+    background-color: #003049;
+    border: #ffffff 1px solid;
+    border-radius: 10px;
+    color: #ffffff;
+    margin-right: 5px;
+  }
+
+  .navbar-register {
+    background-color: #003049;
+    border: #ffffff 1px solid;
+    border-radius: 10px;
+    color: #ffffff;
+    margin-right: 10px;
+  }
+
+  .navbar-title {
+    padding-left: 10px;
+  }
+</style>
--- web/src/pages/index.svelte
+++ web/src/pages/index.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import {url} from "@roxi/routify"
-	let name = "dario";
+	import {url} from "@roxi/routify";
+	import Navbar from "../components/Navbar.svelte"
 </script>
 
+
 <main>
+	<Navbar />
 	<h1>Welcome to expense tracker</h1>
 	<a href={$url("./Register")}>Register</a>
 	<a href={$url("./Login")}>Login</a>
