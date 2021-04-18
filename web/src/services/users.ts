d1a5cbce92be6f17078b1609385823ad7c4284b1
DarioRoman <darioroman2001@gmail.com>
2021-04-18 12:42:58 -0400

    create a generic function to fetch data and types for type safety.

---
+++ web/src/services/users.ts
@@ -0,0 +1,36 @@
+export interface User {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  username: string;
+  email: string;
+}
+
+export interface Expense {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  description: string;
+  amount: number;
+  category: string;
+  user_id: number;
+}
+
+export interface ExpensesResponse {
+  expenses: Expense[];
+  hasMore: boolean;
+}
+
+export default async function api<T>(url: string): Promise<T> {
+  const response = await fetch(url, {
+    credentials: 'include',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+  if (!response.ok) {
+    return;
+  }
+  return await response.json();
+}
