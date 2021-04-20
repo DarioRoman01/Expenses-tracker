34e9919bcffe9388462d522a2a9791e7502f6093
DarioRoman <darioroman2001@gmail.com>
2021-04-20 18:59:44 -0400

    redesigning navbar.

---
+++ web/src/components/Navbar2.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { goto } from '@roxi/routify';
+  import { AppBar, Button, Icon, Menu, ListItem, MaterialApp } from 'svelte-materialify';
+  export let isLoggedIn: boolean;
+
+  async function logout() {
+    const res = await fetch("http://localhost:1323/logout", {
+      method: "post",
+      credentials: "include"
+    });
+    if (res.ok) {
+      $goto("./index");
+    }
+  }
+</script>
+
+<MaterialApp>
+  <AppBar>
+    <span slot="title">Expenses Tracker</span>
+    <div style="flex-grow:1" />
+    {#if isLoggedIn}
+      <Button class="primary-color" on:click={$goto("./create-expense")}>
+        Add
+      </Button>
+      <Button class="ml-3 primary-color" on:click={logout}>
+        logout
+      </Button>
+    {:else}
+      <Button class="primary-color" on:click={$goto("./login")}>
+        Login
+      </Button>
+      <Button class="ml-4 primary-color" on:click={$goto("./register")}>
+        Register
+      </Button>
+    {/if}
+  </AppBar>
+</MaterialApp>
+
