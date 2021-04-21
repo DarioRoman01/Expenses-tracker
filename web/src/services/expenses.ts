2693ff3d30e974b5eff9d6688a54d82333b22d11
Haizza1 <darioroman2001@gmail.com>
2021-04-21 15:24:39 -0400

    adding more requests.

--- web/src/services/expenses.ts
+++ web/src/services/expenses.ts
@@ -44,3 +44,49 @@ export const addExpense = async (
 
   return await res.json();
 };
+
+export const deleteExpense = async (id: number) => {
+  const res = await fetch(`http://localhost:1323/expenses/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message);
+  }
+
+  return await res.json();
+}
+
+export const updateExpense = async (
+  id: number,
+  description?: string,
+  amount?: number,
+  category?: string,
+): Promise<Expense> => {
+  const res = await fetch(`http://localhost:1323/expenses/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      description: description,
+      amount: amount,
+      category: category,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message);
+  }
+
+  return await res.json();
+}
