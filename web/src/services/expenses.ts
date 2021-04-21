96014e74e7eee7dcc7baa0826a4475bfdff08c8f
Haizza1 <darioroman2001@gmail.com>
2021-04-21 08:48:29 -0400

    pulling await fetch methods from svelte files.

---
+++ web/src/services/expenses.ts
@@ -0,0 +1,46 @@
+export interface Expense {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  description: string;
+  amount: number;
+  category: string;
+  user_id: number;
+}
+
+export interface ExpensesResponse {
+  expenses: Expense[];
+  hasMore: boolean;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export const addExpense = async (
+  description: string,
+  amount: number,
+  category: string
+) => {
+  const res = await fetch("http://localhost:1323/expenses", {
+    method: "POST",
+    body: JSON.stringify({
+      description: description,
+      amount: amount,
+      category: category,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message);
+  }
+
+  return await res.json();
+};
--- web/src/services/users.ts
+++ web/src/services/users.ts
@@ -1,3 +1,110 @@
+export default async function api<T>(url: string): Promise<T> {
+  const response = await fetch(url, {
+    credentials: 'include',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+  if (!response.ok) {
+    return;
+  }
+  return await response.json();
+}
+
+export const login =  async ({usernameOrEmail, password}): Promise<User> => {
+  const res = await fetch("http://localhost:1323/login", {
+    method: "post",
+    body: JSON.stringify({
+      usernameOrEmail: usernameOrEmail,
+      password: password,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message);
+  }
+
+  return await res.json();
+}
+
+export const register = async ({username, email, password}): Promise<User> => {
+  const res = await fetch("http://localhost:1323/signup", {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+ 
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message)
+  }
+
+  return await res.json();
+}
+
+// handle users forgot password request
+export const forgotPassword = async (email: string) => {
+  const res = await fetch("http://localhost:1323/forgot-password", {
+    method: "post",
+    body: JSON.stringify({
+      email: email
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message)
+  }
+
+  return await res.json();
+}
+
+// handles users change password request
+export const changePassword = async (
+  token: string, 
+  newPassword: string
+  ): Promise<User> => {
+  const res = await fetch("http://localhost:1323/change-password", {
+    method: "POST",
+    body: JSON.stringify({
+      token: token,
+      newPassword: newPassword
+    }),
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    const err = await res.json();
+    throw new Error(err.message)
+  }
+
+  return await res.json();
+}
+
 export interface User {
   id: number;
   createdAt: string;
@@ -26,16 +133,3 @@ export interface Category {
   name: string;
 }
 
-export default async function api<T>(url: string): Promise<T> {
-  const response = await fetch(url, {
-    credentials: 'include',
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-  if (!response.ok) {
-    return;
-  }
-  return await response.json();
-}
